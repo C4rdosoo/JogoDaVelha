a23fb91adcd1361a2b5e0c5080064258a30d1788
C4rdos027 <164185022+C4rdosoo@users.noreply.github.com>
2025-09-10 19:38:08 -0300

    implenmetação de chegar vitoria

--- program/src/Main.java
+++ program/src/Main.java
@@ -17,6 +17,18 @@ public class Main {
         while (game) {
             desenhaJogo(velha);
 
+            // verifica se houve ganhador
+            vitoria = VAR(velha) ;
+            if(!vitoria.isEmpty()) {
+                System.out.printf("Jogador %s venceu%n" , vitoria);
+                break;
+            }
+
+                // verifica se houve empate
+                if (empate(velha)) {
+                    System.out.println("!! Deu velha !!");
+                    break;
+                }
 
 
             }
@@ -87,4 +99,32 @@ public class Main {
 
 // implementar verificar  vitória
 
+    public static String VAR(campo[][] velha) {
+        // linhas
+        for (int i = 0; i < 3; i++) {
+            char a = velha[i][0].getSimbolo();
+            if (a != ' ' && a == velha[i][1].getSimbolo() && a == velha[i][2].getSimbolo()) return String.valueOf(a);
+        }
+        // colunas
+        for (int j = 0; j < 3; j++) {
+            char a = velha[0][j].getSimbolo();
+            if (a != ' ' && a == velha[1][j].getSimbolo() && a == velha[2][j].getSimbolo()) return String.valueOf(a);
+        }
+        // diagonais
+        char c = velha[1][1].getSimbolo();
+        if (c != ' ' && velha[0][0].getSimbolo() == c && velha[2][2].getSimbolo() == c) return String.valueOf(c);
+        if (c != ' ' && velha[0][2].getSimbolo() == c && velha[2][0].getSimbolo() == c) return String.valueOf(c);
 
+        return "";
+    }
+
+    // verifica empate
+    private static boolean empate(campo[][] velha) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (velha[i][j].getSimbolo() == ' ') return false;
+            }
+        }
+        return VAR(velha).isEmpty();
+    }
+}
